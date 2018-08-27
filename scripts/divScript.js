1b3efb4a58af115b6a4d66f4a0b05084455b92e2
rogiertenhacken <rogiertenhacken@gmail.com>
2018-08-27 16:21:07 +0200

    added navbar transparency on scroll

--- scripts/divScript.js
+++ scripts/divScript.js
@@ -1,3 +1,5 @@
+/* eslint-env jquery */
+
 let allImages = ['Images/codePhotos/codeGroup01.jpg', 'Images/codePhotos/codeHands.jpg', 'Images/codePhotos/codingComputer02.jpg']
 
 for (let i = 0; i < allImages.length; i++) {
@@ -7,3 +9,13 @@ for (let i = 0; i < allImages.length; i++) {
   img.setAttribute('class', 'img-fluid')
   document.getElementById('main').appendChild(div).appendChild(img).src = allImages[i] // write div to page
 }
+
+// to make navbar non-transparent:
+$(window).scroll(function () {
+  // if window is scrolled more than 100px, adds the darken class
+  if ($(this).scrollTop() > 100) {
+    $('.navbar').addClass('darken')
+  } else {
+    $('.navbar').removeClass('darken')
+  }
+})
