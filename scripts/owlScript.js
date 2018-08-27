d469d92cbf4b416f620e017908bfe2d10cf9df45
rogiertenhacken <rogiertenhacken@gmail.com>
2018-08-27 15:48:19 +0200

    minor changes

--- scripts/owlScript.js
+++ scripts/owlScript.js
@@ -1,7 +1,11 @@
 /* eslint-env jquery */
 
 $('.owl-carousel').owlCarousel({
+  items: 3,
   loop: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
   margin: 0,
   nav: false,
   dots: false,
